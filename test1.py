70194a6c4fc22e840bdeec5ffc842eee196e6452
//nickname <45104360+nicknamehub@users.noreply.github.com>
2018-11-16 20:13:22 +0300

    Delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome()
-subject = []
-url_start = 'https://olimpiada.ru/activities?type=any&subject%5B'
-url_end = '%5D=on&class=any&period_date=&period=year'
-for i in range(34):
-    driver.get(url_start + str(i) + url_end)
-    string = driver.find_element_by_id('megatitle').text
-driver.close()
